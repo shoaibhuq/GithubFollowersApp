526c01397c69b41eafce6b530dcc485ba2094fa9
Shoaib Huq <slimsho123@gmail.com>
2022-08-01 15:32:10 -0500

    first commit

---
+++ GithubFollowers/VCs/FavoritesVC.swift
@@ -0,0 +1,29 @@
+//
+//  FavoritesVC.swift
+//  GithubFollowers
+//
+//  Created by Shoaib Huq on 7/29/22.
+//
+
+import UIKit
+
+class FavoritesVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
